83393a5da311e32a4a3755d111361c8641206048
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2026-02-10 00:50:14 +0000

    v1 changes and deployment pushed from mobile

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "income-tracker-v1";
+const ASSETS = [
+  "./",
+  "./index.html"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
